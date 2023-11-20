a288962b84a34d828611b586cee94de73d190e7a
Zan <minhthinh130202@gmail.com>
2023-11-20 11:18:11 +0700

    Discriminate handle on FE and BE, change UI of error

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -7,6 +7,7 @@ const LoginForm = () => {
     const [messageEmail, setMessageEmail] = useState("");
     const [messagePassword, setMessagePassword] = useState("");
 
+
     const handleEmailOnChange = (e) => {
         setEmail(e.target.value);
     }
@@ -16,16 +17,47 @@ const LoginForm = () => {
     }
 
     const handleOnClickLogin = (e) => {
+        let flag = true
         const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
         if (regex.test(email))
-            setMessageEmail("Email is Valid")
-        else
+            setMessageEmail("")
+        else {
             setMessageEmail("Email is Invalid")
+            flag = false
+        }
 
-        if (password === "")
+
+        if (password === "") {
             setMessagePassword("Missing Password")
+            flag = false
+        }
+
         else
             setMessagePassword("")
+
+        const info = {
+            "email": email,
+            "password": password
+        }
+
+        var responses
+        if (flag) {
+            fetch("http://127.0.0.1:8000/users/auth", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(info)
+            }).then((response) => {
+                responses = response
+                console.log(response)
+                if (response.status === 200)
+                    alert("Login successful")
+                else
+                    alert("Unable Username and Password")
+            }).catch(rejected => {
+                console.log(rejected);
+            });
+        }
+
 
     }
 
@@ -33,13 +65,22 @@ const LoginForm = () => {
     return (
         <div className="cover">
             <h1> Welcome</h1>
-            <input
-                type="text" placeholder="Email" value={email} onChange={handleEmailOnChange}>
-            </input>
-            <span>{messageEmail}</span>
+            <div className="login-input">
+                <input
+                    type="text" placeholder="Email" value={email} onChange={handleEmailOnChange}>
+                </input>
+                {
+                    messageEmail ? <span>*{messageEmail}</span> : <span className="hide"></span>
+                }
+            </div>
 
-            <input type="password" placeholder="Password" value={password} onChange={handlePasswordOnChange}></input>
-            <span>{messagePassword}</span>
+            <div className="login-input">
+                <input type="password" placeholder="Password" value={password} onChange={handlePasswordOnChange}></input>
+                {
+                    messagePassword ? <span>*{messagePassword}</span> : <span className="hide"></span>
+                }
+            </div>
+
             <button className="login-btn" onClick={handleOnClickLogin}>Login</button>
 
             <div className="alt-login">
